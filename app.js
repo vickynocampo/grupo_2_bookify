167f77a838f5e54c282e0c5c73f0b1b4aade37c4
vickynocampo <vickynocampo@hotmail.com>
2022-06-04 20:17:47 -0300

    Se agrega express y estructuras de hmtl

---
+++ app.js
@@ -0,0 +1,15 @@
+let express = require("express");
+let path = require("path");
+let app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.get("/",(req, res)=>{res.sendFile(path.resolve(__dirname, "./views/index.html"))});
+app.get("/productDetail",(req, res)=>{res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))});
+app.get("/productCart",(req, res)=>{res.sendFile(path.resolve(__dirname, "./views/productCart.html"))});
+app.get("/register",(req, res)=>{res.sendFile(path.resolve(__dirname, "./views/register.html"))});
+app.get("/login",(req, res)=>{res.sendFile(path.resolve(__dirname, "./views/login.html"))});
+
+app.listen(3000,()=>{console.log("Servidor corriendo en puerto 3030");})
+
