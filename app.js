f60b867c031df69a0ad0d368c2443c9dcff86610
sebasbente <sebasbentedg@gmail.com>
2022-06-11 21:40:39 -0300

    css footer

--- app.js
+++ app.js
@@ -11,5 +11,11 @@ app.get("/productCart",(req, res)=>{res.sendFile(path.resolve(__dirname, "./view
 app.get("/register",(req, res)=>{res.sendFile(path.resolve(__dirname, "./views/register.html"))});
 app.get("/login",(req, res)=>{res.sendFile(path.resolve(__dirname, "./views/login.html"))});
 
-app.listen(3030,()=>{console.log("Servidor corriendo en puerto 3030");})
+app.post("/productCart",(req, res)=>{res.redirect("/")});
+app.post("/register",(req, res)=>{res.redirect("/")});
+app.post("/login",(req, res)=>{res.redirect("/")});
+// app.post("/productDetail",(req, res)=>{res.redirect("/")});
+
+app.listen(3030,()=>{console.log("Servidor corriendo en puerto 3030")});
+
 
