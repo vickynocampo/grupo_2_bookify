45a69dae47ce401139fe6c39059444afcd4e9d79
vickynocampoemail <you@example.com>
2022-07-05 20:26:13 -0300

    productDetail ejs terminada, index ejs terminada, Json con libros de Arte e Historia. Carrousel pendinete

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -6,19 +6,28 @@ const books = JSON.parse(fs.readFileSync(booksFilePath, 'utf-8'));
 
 
 const productsController = {
-    detail :(req, res)=>{res.render("productDetail")},
-    // app.post("/productDetail",(req, res)=>{res.redirect("/")});
+    detail: (req, res) => {
+      
+        let actualBookCat = req.paramas.id
+        let similares = [];
+        for (let i = 0; i < books.length; i++) {
+            if (books[i].bookCategory === actualBookCat) {
+                enOferta.push(similares[i])
+            }
+        }
+        res.render("productDetail", { similar: similares })},
 
-    create: (req, res) => {res.render("productCreate")},
-    edit: (req, res) => {res.render("productEdit")},
+
+    create: (req, res) => { res.render("productCreate") },
+    edit: (req, res) => { res.render("productEdit") },
     getProductById: (req, res) => {
-        const bookId = parseInt(req.params.id,10);
+        const bookId = parseInt(req.params.id, 10);
         let bookFounded = "";
-        for (let i=0; i<=books.length; i++) {
+        for (let i = 0; i <= books.length; i++) {
             if (books[i].id === bookId) {
                 bookFounded = books[i]
-                res.render("productDetail", {book: bookFounded})
-            } 
+                res.render("productDetail", { book: bookFounded })
+            }
         }
     }
 }
