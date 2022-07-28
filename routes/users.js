a388de8e6d55b2c4579fd81a47ee4f3b123d1e38
vickynocampoemail <you@example.com>
2022-07-28 18:58:09 -0300

    login de usuarios en proceso, sin funcionar el redirect final

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const userController = require("../controllers/usersController");
 const router = express.Router();
 const usersController = require("../controllers/usersController");
 const upload = require("../middlewares/multerUsersMiddleware");
@@ -6,12 +7,12 @@ const validations = require('../middlewares/validationUserMiddleware');
 
 router.get("/register", usersController.registerView);
 
+router.post("/register", upload.single('avatar'), validations, usersController.registerProcess);
+
 router.get("/login", usersController.login);
 
+router.post("/login", usersController.loginProcess);
+
 router.get("/:id/detail", usersController.editUser);
-
-router.post("/login", usersController.loginPost);
-
-router.post("/register", upload.single('avatar'), validations, usersController.registerProcess);
 
 module.exports = router;
